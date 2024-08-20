2b637af4ac2809cfe2090514beb96b3123b68acb
SinmisolaE <sinmisolaajao@gmail.com>
2024-08-20 11:59:43 +0000

    0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,4 @@
-//displayMessage that prints in STDOUT the string argument
+// displayMessage that prints in STDOUT the string argument
 
 function displayMessage(message) {
   console.log(message);
