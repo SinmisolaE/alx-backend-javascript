24c083d236a9bd9852754a2d6eeab8280e21f777
SinmisolaE <sinmisolaajao@gmail.com>
2024-08-20 12:33:57 +0000

    1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+// 1-stdin.js that will be executed through command line
+
+console.log("Welcome to Holberton School, what is your name?");
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is ${data}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is ${data}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+  console.log("This important software is now closing");
+  });
+}
